607033a52a751045748f56592d42c8b9c6634a77
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-05-25 22:23:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # Same: self.end_with?(".")
+    end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).count { |sentence| sentence.size != 0 }
   end
 end
